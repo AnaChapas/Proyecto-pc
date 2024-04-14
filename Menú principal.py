2171b6ddc6a0e01bc6d16359360b7eff22983c1d
AnaChapas <agchapasc@correo.url.edu.gt>
2024-04-13 22:03:19 -0600

    Se realizaron cambios en la parte print de menú

--- Menú principal.py
+++ Menú principal.py
@@ -1,7 +1,9 @@
-print("Bienvenido al programa Gasolineras Jaguar")
-print("Elige una de las siguientes opciones:")
-print(" Escribe 1 si desea gestionar inventario")
-print(" Escribe 2 si desea comprar combustible")
-print(" Escribe 3 si desea gestionar turnos")
-print(" Escribe 4 si desea realizar el reporte de rentabilidad")
-eleccion= input(" Coloque la opcion elegida: ")
+continuar=True
+while continuar:
+    print("Bienvenido al programa Gasolineras Jaguar")
+    print("Elige una de las siguientes opciones:")
+    print(" Escribe (1) si desea gestionar inventario")
+    print(" Escribe (2) si desea comprar combustible")
+    print(" Escribe (3) si desea gestionar turnos")
+    print(" Escribe (4) si desea realizar el reporte de rentabilidad")
+    eleccion= int(input(" Coloque la opcion elegida: "))
