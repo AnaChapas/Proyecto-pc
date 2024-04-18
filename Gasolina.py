08595fce844698cc87a2aae79d9269e1273f961a
AnaChapas <agchapasc@correo.url.edu.gt>
2024-04-18 16:44:22 -0600

    Se agregaron los codigos de las primeras 3 partes del menu, tambien se eliminaron los documentos que fueron de prueba para cada uno de los codigos

--- Gasolina.py
+++ Gasolina.py
@@ -1,22 +1,25 @@
-#Menú de combustible
-
 #VARIABLES GENERALES
 #Cantidad inicial en cada deposito
 depositoR=40
 depositoS=40
 depositoD=40
-#Contador para cantidad de galones final de deposito
-fdepositoR=40
-fdepositoS=40
-fdepositoD=40
+#Costos de gestion de inventario 
+costoregular = 7.00
+costosuper = 8.00
+costodiesel = 6.00
 #Precios de gasolina
 Pregular=29.00
 Psuper=30.00
 Pdiesel=26.50
-#Variables para precio 
-precioagregar=0
-precioventa=0
-
+#Capacidad maxima de galones:
+capacidad_maxima = 80
+#Variables de gestion de turnos
+tradiurna=1
+travespe=1
+tranoc=1
+saldi=14.00
+salves=14.50
+salnoc=15.50
 #CODIGOS 
 continuar=True
 while continuar:
@@ -29,9 +32,60 @@ while continuar:
     print(" Escribe 4 si desea realizar el reporte de rentabilidad")
     eleccion= int(input(" Coloque la opcion elegida: "))
 #Codigo de gestion de inventario
+    if eleccion==1:
+        print("Menú:")
+        print("1. Gestionar inventario")
+        print("2. Salir")
+        opcion = input("Seleccione una opción: ")
+        if opcion == '1':
+            print("Nivel de los depósitos:")
+            print(f"Regular: {depositoR} galones")
+            print(f"Súper: {depositoS} galones")
+            print(f"Diésel: {depositoD} galones")
+
+            agregar_combustible = input("¿Desea agregar combustible? (s/n): ")
+
+            if agregar_combustible.lower() == 's':
+                cantregular = float(input("Ingrese la cantidad de galones de gasolina regular: "))
+                fdepositoR=depositoR++cantregular
+                if fdepositoR <= capacidad_maxima:
+                    fcostoR = (cantregular * costoregular)
+                else:
+                    fdepositoR=depositoR
+                    print("Operación inválida. La cantidad excede la capacidad del depósito de gasolina regular.")
+                    fcostoR = 0
+                
+                cantsuper = float(input("Ingrese la cantidad de galones de gasolina súper: "))
+                fdepositoS=depositoS++cantsuper
+                if fdepositoS <= capacidad_maxima:
+                    fcostoS = (cantsuper * costosuper)
+                else:
+                    fdepositoS=depositoS
+                    print("Operación inválida. La cantidad excede la capacidad del depósito de gasolina súper.")
+                    fcostoS = 0
+            
+                cantdiesel = float(input("Ingrese la cantidad de galones de diésel: "))
+                fdepositoD=depositoD++cantdiesel
+                if fdepositoD <= capacidad_maxima:
+                    fcostoD = (cantdiesel * costodiesel)                  
+                else:
+                    fdepositoD=depositoD
+                    print("Operación inválida. La cantidad excede la capacidad del depósito de diésel.")
+                    fcostoD = 0
+                         
+                print(f"Se agregó combustible:")
+                print(f"Costo de almacenamiento de gasolina regular: Q{fcostoR:.2f}")
+                print(f"Costo de almacenamiento de gasolina súper: Q{fcostoS:.2f}")
+                print(f"Costo de almacenamiento de diésel: Q{fcostoD:.2f}")
+
+        elif opcion == '2':
+            print("Gracias por utilizar nuestros servicios, vuelva pronto")
+            break
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
 
 #Codigo de venta de combustible
-    if eleccion == 2:
+    elif eleccion == 2:
             print("")
             print("--------------CARTELERA Y GALONES DISPONIBLES EN CADA DEPOSITO--------------")
             print("El precio de Regular es: Q.",Pregular, " el deposito de Regular tiene:",fdepositoR," galones") 
@@ -103,6 +157,7 @@ while continuar:
                                     else:
                                         print("Número de tarjeta inválido. Intente nuevamente.")
                                 else:
+                                    fdepositoR=fdepositoR++eleccion4
                                     print("")
                                     print("¿Desea regresar a menú?")
                                     print("Si desea regresar a menú, coloque 1")
@@ -115,7 +170,7 @@ while continuar:
                     else:
                         print("")
                         eleccion10=int(input("¿cuantos desea agregar? "))      
-                        galones1=eleccion10/fdepositoR  
+                        galones1=eleccion10/Pregular  
                         fdepositoR=fdepositoR-galones1        
                         if fdepositoR<0:
                             fdepositoR=fdepositoR++galones1
@@ -152,6 +207,7 @@ while continuar:
                                     else:
                                         print("Número de tarjeta inválido. Intente nuevamente.")
                                 else:
+                                    fdepositoR=fdepositoR++galones1
                                     print("")
                                     print("¿Desea regresar a menú?")
                                     print("Si desea regresar a menú, coloque 1")
@@ -222,6 +278,7 @@ while continuar:
                                         else:
                                             print("Número de tarjeta inválido. Intente nuevamente.")
                                     else:
+                                        fdepositoS=fdepositoS++eleccion17
                                         print("")
                                         print("¿Desea regresar a menú?")
                                         print("Si desea regresar a menú, coloque 1")
@@ -234,7 +291,7 @@ while continuar:
                         else:
                             print("")
                             eleccion23=int(input("¿cuanto desea agregar? "))      
-                            galones3=eleccion23/fdepositoS 
+                            galones3=eleccion23/Psuper 
                             fdepositoS= fdepositoS-galones3         
                             if fdepositoS<0:
                                 fdepositoS=fdepositoS++galones3
@@ -272,6 +329,7 @@ while continuar:
                                         else:
                                             print("Número de tarjeta inválido. Intente nuevamente.")
                                     else:
+                                        fdepositoS=fdepositoS++galones3
                                         print("")
                                         print("¿Desea regresar a menú?")
                                         print("Si desea regresar a menú, coloque 1")
@@ -343,6 +401,7 @@ while continuar:
                                     else:
                                         print("Número de tarjeta inválido. Intente nuevamente.")
                                 else:
+                                    fdepositoD=fdepositoD++eleccion29
                                     print("")
                                     print("¿Desea regresar a menú?")
                                     print("Si desea regresar a menú, coloque 1")
@@ -355,7 +414,7 @@ while continuar:
                     else:
                         print("")
                         eleccion34=int(input("¿cuanto desea agregar? "))      
-                        galones5=eleccion34/fdepositoD    
+                        galones5=eleccion34/Pdiesel   
                         fdepositoD=fdepositoD-galones5     
                         if fdepositoD<0:
                             fdepositoD=fdepositoD++galones5
@@ -393,6 +452,7 @@ while continuar:
                                     else:
                                         print("Número de tarjeta inválido. Intente nuevamente.")
                                 else:
+                                    fdepositoD=fdepositoD++galones5
                                     print("")
                                     print("¿Desea regresar a menú?")
                                     print("Si desea regresar a menú, coloque 1")
@@ -403,5 +463,86 @@ while continuar:
                                     else:
                                         continuar = False
 #Codigo de gestión de turnos
-salariotrabajadores=0
+    elif eleccion==3:
+        print("")
+        print("--------------------------Gestión de turnos-------------------------------")
+        print("--------Tipo de jornada------------------------Total de trabajadores------")
+        print("----------- Diurna -------------------------------- ",tradiurna," --------")
+        print("--------- Vespertina ------------------------------ ",travespe," ---------")
+        print("---------- Nocturna ------------------------------- ",tranoc," -----------")
+        print("--------------------------------------------------------------------------")
+        print("")
+        print("¿Desea agregar o retirar personal a alguna jornada?")
+        print("Si desea agregar personal, coloque 1")
+        print("Si desea retirar personal, coloque 2")
+        per= int(input(" Coloque la opcion elegida: "))
+        if per==1:
+            print("")
+            per1=int(input("¿Cuantas personas desea agregar a la jornada diurna?: "))
+            tradiurna=per1++tradiurna
+            per2=int(input("¿Cuantas personas desea agregar a la jornada vespertina?: "))
+            travespe=travespe++per2
+            per3=int(input("¿Cuantas personas desea agregar a la jornada nocturna?:"))
+            tranoc=per3++tranoc
+            print("")
+            print("----------------------------Costos de los trabajadores-----------------------------")
+            print("-----------Horarios------------Tipo de jornada------------Salario por jornada------")
+            print("------ 12:00 AM - 8:00 AM --------- Diurna ------------------- ",saldi," ----------")
+            print("------ 8:00 AM - 4:00 PM --------- Vespertina --------------- ",salves," ----------")
+            print("------ 4:00 PM - 12:00 AM --------- Nocturna ---------------- ",salnoc," ----------")
+            print("-----------------------------------------------------------------------------------")
+            print("¿Desea regresar a menú?")
+            print("Si desea regresar a menú, coloque 1")
+            print("Si desea salir definitivamente del programa, coloque 2")
+            print("Si desea salir definitivamente del programa, coloque 2")
+            eleccion60=int(input("Coloque la opcion elegida: "))
+            if eleccion60==1:
+                continuar=True
+            else:
+                continuar=False
+ 
+        else:
+            print("")
+            per4=int(input("¿Cuantas personas desea retirar de la jornada diurna?: "))
+            tradiurna=tradiurna-per4
+            if tradiurna<=0:
+                tradiurna=tradiurna++per4
+                print("La cantidad de empleados que desea retirar no es permitida")
+            else:
+                tradiurna
+            
+            per6=int(input("¿Cuantas personas desea retirar de la jornada vespertina?: "))
+            travespe=travespe-per6
+            if travespe<=0:
+                travespe=travespe++per6
+                print("La cantidad de empleados que desea retirar no es permitida")
+            else:
+                travespe
+
+            per8=int(input("¿Cuantas personas desea retirar de la jornada Nocturna?: "))
+            tranoc=tranoc-per8
+            if tranoc<=0:
+                tranoc=tranoc++per8
+                print("La cantidad de empleados que desea retirar no es permitida")
+            else:
+                tranoc
+                
+            print("")
+            print("----------------------------Costos de los trabajadores-----------------------------")
+            print("-----------Horarios------------Tipo de jornada------------Salario por jornada------")
+            print("------ 12:00 AM - 8:00 AM --------- Diurna ------------------- ",saldi," ----------")
+            print("------ 8:00 AM - 4:00 PM --------- Vespertina --------------- ",salves," ----------")
+            print("------ 4:00 PM - 12:00 AM --------- Nocturna ---------------- ",salnoc," ----------")
+            print("-----------------------------------------------------------------------------------")
+            print("")
+            print("¿Desea regresar a menú?")
+            print("Si desea regresar a menú, coloque 1")
+            print("Si desea salir definitivamente del programa, coloque 2")
+            eleccion70=int(input("Coloque la opcion elegida: "))
+            if eleccion70==1:
+                continuar=True
+            else:
+                continuar=False
 #Codigo de reporte de rentabilidad
+    else:
+        print("")
